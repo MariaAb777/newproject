551ed24ea1ffec1bae5d825d875879bf00fe19b4
MariaAb777 <mabrahayman666@gmail.com>
2021-01-27 21:52:49 +0400

    created modal page with modal

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,43 @@
       <router-link to="/Notte">Note</router-link> |
       <router-link to="/Modal">Modal</router-link>
     </div>
-    <router-view/>
+
+    <div class="wrapper">
+      <div class="wrapper_content">
+        <section>
+          <div class="container">
+            <button @click="modalFirst = !modalFirst" class="btn">Show first modal</button>
+            <modal title="first modal"
+                    v-if="modalFirst"
+                    @close='modalFirst = !modalFirst'
+                    
+                    
+                    >
+                   
+             </modal>
+          </div>
+        </section>
+      </div>
+    </div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 
 <script>
-
-
+import modal from "./views/Modals/Modal.vue";
 
 export default {
-  name: 'App',
-  data(){
-    return{
-
-    }
-  }
-}
+  name: "App",
+  components: {
+    modal,
+  },
+  data() {
+    return {
+      modalFirst: false,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
